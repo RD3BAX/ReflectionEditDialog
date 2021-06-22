96dc8650d2c3345be86da425769b5903dd00f3a0
Magistr <MagistrTNT@msn.com>
2021-06-22 20:39:54 +0300

    Добавьте файлы проекта.

---
+++ ReflectionEditDialog/App.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ReflectionEditDialog.ViewModels;
+
+namespace ReflectionEditDialog
+{
+    public partial class App
+    {
+        public static Window FocusedWindow => Current.Windows.Cast<Window>().FirstOrDefault(w => w.IsFocused);
+
+        public static Window ActiveWindow => Current.Windows.Cast<Window>().FirstOrDefault(w => w.IsActive);
+
+        public static Window CurrentWindow => FocusedWindow ?? ActiveWindow ?? Current.MainWindow;
+
+        private static IHost _Hosting;
+        public static IHost Hosting => _Hosting
+            ??= CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        public static IServiceProvider Services => Hosting.Services;
+
+        public static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration(opt => opt.AddJsonFile("appsettings.json", true, true))
+            .ConfigureServices(ConfigureServices);
+
+        private static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+            services.AddViewModels();
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            var host = Hosting;
+            base.OnStartup(e);
+            await host.StartAsync();
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            using var host = Hosting;
+            base.OnExit(e);
+            await host.StopAsync();
+        }
+    }
+}
